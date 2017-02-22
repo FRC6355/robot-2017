4081da776b879b66d9b5a4ac655832f75685e515
Julia <jjavorsky17@hb.edu>
2017-02-21 20:30:20 -0500

    fixed flywheel

--- RobotsOverParma/src/org/usfirst/frc6355/RobotsOverParma/commands/StartFlywheelCommand.java
+++ RobotsOverParma/src/org/usfirst/frc6355/RobotsOverParma/commands/StartFlywheelCommand.java
@@ -53,11 +53,12 @@ public class StartFlywheelCommand extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.shooter.stopShooter();
+    	//Robot.shooter.stopShooter();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	Robot.shooter.stopShooter();
     }
 }
