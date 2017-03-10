2de7bc698223f39bedac438f33f029ede5d74b3e
Nathan Allen-Wagner <nathan@alner.net>
2017-03-09 22:15:11 -0500

    Added support for an Autonomous Command.
    - Drive forward, turn, drive forward, drive back.
    - Uses the NavX to turn to the desired angle.
    - Has been tested on the mule robot.
    - Adjusted the DriveTrain to support the desired features.
    - Added "deadband" support in the drivetrain.
    - Added two parameterized commands for driving forward for a certain
    number of seconds. And another for turning to the desired angle.
    - Added comments to the code to explain the intent.

---
+++ RobotsOverParma/src/org/usfirst/frc6355/RobotsOverParma/commands/TurnToAngleCommand.java
@@ -0,0 +1,69 @@
+package org.usfirst.frc6355.RobotsOverParma.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc6355.RobotsOverParma.Robot;
+import org.usfirst.frc6355.RobotsOverParma.RobotMap;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Timer;
+
+/**
+ *
+ */
+public class TurnToAngleCommand extends Command {
+
+	private double startTime = 0.0;
+	private double degreesToTurn = 0.0;
+	private double driveMagnitude = 0.0;
+	private double degreesWhenFinished = 0.0;
+
+	public TurnToAngleCommand(double degreesToTurn, double driveMagnitude) {
+    	requires(Robot.driveTrain);
+		System.out.println("Turn command created.");
+		this.degreesToTurn = degreesToTurn;
+		this.driveMagnitude = driveMagnitude;
+	}
+
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		startTime = Timer.getFPGATimestamp(); // seconds.
+
+		Robot.driveTrain.setAngle(this.degreesToTurn);
+
+		this.degreesWhenFinished = (Robot.driveTrain.getAngle() + this.degreesToTurn) % 360;
+		System.out.println("Turn command init. Angle:" + Robot.driveTrain.getAngle() + " \r\nDegrees to Turn: " 
+				+ degreesToTurn + ". \r\nDegrees when Done: " + degreesWhenFinished);
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		Robot.driveTrain.TurnToSetpoint(this.driveMagnitude);
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		double currentTime = Timer.getFPGATimestamp();
+
+		double degreesFromTargetAngle = degreesWhenFinished - Robot.driveTrain.getAngle();
+
+		if (Math.abs(degreesFromTargetAngle) < 3.0) {
+			System.out.println("Turn command finished. Current Degrees: " + Robot.driveTrain.getAngle() + " Off from Target: "
+					+ degreesFromTargetAngle);
+			return true;
+		}
+
+		return false;
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+		System.out.println("Turn command end.");
+		Robot.driveTrain.Stop();
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+		end();
+	}
+}
